23411a8ac107410047fc2597c0c50ec9352265ae
GEN-004 <gen-004@gen004-HP-ProBook-450-G5>
2019-05-03 00:22:26 +0200

    shifumi fini

--- DOM.js
+++ DOM.js
@@ -1,109 +1,125 @@
-// var tjs en haut de page //
-// var laDivision = document.getElementById("ok");
-// var nok = document.getElementsByClassName("identique");
-
-
-
-// laDivision.innerHTML = "Hello world";
-// identique.innerHTML = "J'aime le code !!!"
-
-// ******ARRAY (liste)
-// var desApprenants = ["Fabien", "Edains", "Quentin"];
-// console.log(desApprenants.push("Christophe"));
-// console.log(desApprenants[3]);
-
-//*************Exercice carré rose *************//
-// div 250 x 250
-// au clic 500 x 500
-// au clic 250 x 250
-
-// var rose = document.getElementById("rose");
-// var isActivated = false;
-
-// rose.addEventListener("click",function (){
-//         if (isActivated === false) {
-//             rose.style.backgroundColor = "red";
-//             rose.style.width = "500px";
-//             rose.style.height = "500px";
-//             isActivated = true;
-//         } else {
-//             rose.style.backgroundColor = "deeppink";
-//             rose.style.width = "250px";
-//             rose.style.height = "250px";
-//             isActivated = false;
-//         }
-//     }
-// );
-//************************Exercice carré rouge**********************************
-// var rouge = document.getElementById("rouge");
-
-
-// btnJaune.addEventListener("click", function (){
-//     rouge.style.backgroundColor = "yellow";
-//     }
-// );
-
-// btnBleu.addEventListener("click", function (){
-//     rouge.style.backgroundColor = "blue";
-//     }
-// );
-
-// btn300px.addEventListener("click", function (){
-//     rouge.style.width = "300px";
-//     rouge.style.height = "300px";
-// }
-// );
-
-// btnErase.addEventListener("click", function (){
-//     rouge.style.visibility = "hidden";
-//     }
-// );
-
-// btnRéinitialiser.addEventListener("click", function (){
-//     rouge.style.visibility = "visible";
-//     rouge.style.width = "100px";
-//     rouge.style.height = "100px";
-//     rouge.style.backgroundColor = "red";
-//     }
-// );
-
 //************************SHI-FU-MI**********************************
 //
+//function shifumi() {
+    //Déclaration des variables
+    
+    // var iaChoice;
+    // var playerChoice;
+    // var winnerIs;
+    
+    //Déclaration des éléments du DOM
+        
+    //TODO: Fonction Jeu
+      //TODO: Choix de l'IA -> iaChoice
+      //TODO: récupérer le choix du joueur -> playerChoice
+      //TODO: Comparer les choix (playerChoice, iaChoice)-> winnerIs
+         //TODO: Si : Cas égalité
+         //TODO: Sinon Si : Cas de victoire 
+         //TODO: Sinon : Cas de défaite
+      //TODO: Afficher les résultats
+      //TODO: Afficher le choix du joueur (playerChoice)
+        //TODO: Afficher le choix de l'IA (iaChoice)
+        //TODO: Afficher le résultat
+    //TODO: Fonction restart
+    //}
+    
 // 
-// l'utilisateur choisit et clic  entre pierre ou feuille ou ciseaux
 //
-// résultat doit s'afficher dans le cadre "Vous"
 //
-// PC choisit entre pierre ou feuille ou ciseaux
 //
-// on compare choixVous et choixIA
+//      Fonction restart
 //
-// Si choixvous = choixIA
-//      affiché dans résultat Egalité
-//
-// Si choixvous = ciseaux et choixIA = papier
-// Si choixvous = papier et choixIA = caillou
-// Si choixvous = caillou et choixIA = ciseaux
-//      afficher dans résultat Gagné // 
 // 
-// Si choix utilisateur = papier et  choix pc = ciseaux
-// Si choix utilisateur = caillou et  choix pc = papier
-// Si choix utilisateur = ciseaux et  choix pc = caillou
-//      afficher dans résultat Perdu //  
-
-var choixVous = document.getElementsByClassName("vous");
-var choixIA = document.getElementsByClassName("I.A");
-var btnCaillou = document.getElementById("caillou");
-var btnPapier = document.getElementById("papier");
-var btnCiseaux = document.getElementById("ciseaux");
 
 
 
-function compare(choixVous , choixIA) {
-    if ((choixVous == ciseaux) && (choixIA == papier) || (choixVous == papier) && (choixIA == caillou) || (choixVous == caillou) && (choixIA == ciseaux)) {
-    console.log("Gagné !!!");
-    }
-    else if ((choixIA == ciseaux) && (choixVous == papier) || (choixIA == papier) && (choixVous == caillou) || (choixIA == caillou) && (choixVous == ciseaux)) {
-    console.log("Perdu !!!");
-    }
+// Les Variables //
+var pierre = document.getElementById("pierre");
+var feuille = document.getElementById("feuille");
+var ciseaux = document.getElementById("ciseaux");
+
+var choixIA;
+var choixDuIa;
+var choixDuJoueur;
+var choixJoueur;
+var resultat;
+
+var nouvellePartie = document.getElementById("boutonReJouer");
+
+// Faire choisir l'utilisateur et affiché sont choix //
+pierre.addEventListener("click",function choixPierre() {  
+    choixDuJoueur = "pierre";
+    choixJoueur =0;
+    document.getElementById("joueur").style.backgroundImage="url('images/caillou.png')";
+    console.log(choixDuJoueur + choixJoueur);
+    choixDeIa();
+    comparer();
+});
+
+feuille.addEventListener("click",
+function choixFeuille() {      
+    choixDuJoueur = "feuille";
+    choixJoueur =1;
+    document.getElementById("joueur").style.backgroundImage="url('images/papier.png')";
+    console.log(choixDuJoueur + choixJoueur);
+    choixDeIa();
+    comparer();   
+});
+
+ciseaux.addEventListener("click",
+function choixCiseaux() {       
+    choixDuJoueur ="ciseaux";
+    choixJoueur =2;
+    document.getElementById("joueur").style.backgroundImage="url('images/ciseaux.png')";
+    console.log(choixDuJoueur + choixJoueur);  
+    choixDeIa();
+    comparer(); 
+});
+
+
+// Faire choisir l'IA //
+function choixDeIa() {
+    choixIA = Math.floor(Math.random() * 3);
+    choixIaResultat();
 }
+
+// Faire choisir l'IA et montrer le choix //
+function choixIaResultat() {
+    if (choixIA == 0 ) {
+        choixDuIa = "PIERRE";
+        document.getElementById("afficheIa").style.backgroundImage="url('images/caillou.png')";
+    }
+    else if(choixIA  === 1) {
+        choixDuIa= "FEUILLE";
+        document.getElementById("afficheIa").style.backgroundImage="url('images/papier.png')";
+    }
+    else if(choixIA === 2) {
+        choixDuIa = "CISEAUX";
+        document.getElementById("afficheIa").style.backgroundImage="url('images/ciseaux.png')";        
+
+
+
+    }
+};
+console.log(choixDuIa);
+
+// Comparer les choix //
+function comparer() {
+    if (choixJoueur === choixIA) {
+        //console.log("Egalité");
+        afficheResultat.innerHTML = "Egalité !"; 
+    }
+    else if ((choixJoueur==0) && (choixIA==2) || (choixJoueur==1) && (choixIA==0) || (choixJoueur==2) && (choixIA==1)){
+        //console.log("Vous avez gagnez");
+        afficheResultat.innerHTML = "Gagné !";
+    } else {
+        //console.log("Vous avez perdus");
+        afficheResultat.innerHTML = "Perdu !";
+    }
+};
+
+// Relancer le jeu //
+nouvellePartie.addEventListener("click",function(){
+    window.location.reload(false);  
+});
+
